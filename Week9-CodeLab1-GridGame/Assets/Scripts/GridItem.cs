224acefaf6e79c2846daa5dce7cb8a88497cc618
Yongkun Li <indusdesign@163.com>
2020-04-19 14:22:42 -0400

    color changing updated

--- Week9-CodeLab1-GridGame/Assets/Scripts/GridItem.cs
+++ Week9-CodeLab1-GridGame/Assets/Scripts/GridItem.cs
@@ -6,11 +6,19 @@ public class GridItem : MonoBehaviour
 {
     public Material[] materials;
     public int gridX, gridY;
+    public int materialIndex = 0;
 
     void Start()
     {
+        // materialIndex = Random.Range(0, materials.Length);
         GetComponent<MeshRenderer>().material = 
-            materials[Random.Range(0, materials.Length)];
+            materials[materialIndex];
+    }
+    private void Update() {
+        // right click
+        if(Input.GetMouseButtonDown(1)){
+            changeColor();
+        }
     }
 
     // Start is called before the first frame update
@@ -18,7 +26,6 @@ public class GridItem : MonoBehaviour
     {
         gridX = x;
         gridY = y;
-
         name = "X: " + x + " Y: " + y;
     }
 
@@ -30,7 +37,30 @@ public class GridItem : MonoBehaviour
         } else {
             GridManager.instance.Swap(this);
         }
-
         print(name);
+    }
+
+    void changeColor(){
+        // use ray casting to detect which block is chosen
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+        if ( Physics.Raycast (ray,out hit)) {
+            if(hit.transform.position==GetComponent<Transform>().position){
+                IEnumerator coroutine;
+                coroutine = ScaleMe(hit.transform, hit);
+                StartCoroutine(coroutine);
+            }
+        }
+    }
+    IEnumerator ScaleMe(Transform objTr, RaycastHit hit) {
+        objTr.localScale *= 1.2f;
+        yield return new WaitForSeconds(0.3f);
+        this.materialIndex++;
+        if (this.materialIndex>=materials.Length){
+            this.materialIndex = 0;
+        }
+        GetComponent<MeshRenderer>().material = 
+            materials[this.materialIndex];
+        objTr.localScale /= 1.2f;
     }
 }
