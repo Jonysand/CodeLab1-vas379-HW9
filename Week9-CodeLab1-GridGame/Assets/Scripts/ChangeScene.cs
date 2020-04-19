f14b05ed698051f49160e5dce85572069be5367a
RosaitaW <55056093+RosaitaW@users.noreply.github.com>
2020-04-19 16:28:31 -0400

    Updated solution generating script, new scenes and jumping scenes script.

---
+++ Week9-CodeLab1-GridGame/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ChangeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);//Load the next level
+    }
+    public void PlayAgain() {
+        SceneManager.LoadScene(0);//Back to the first scene
+    }
+}
